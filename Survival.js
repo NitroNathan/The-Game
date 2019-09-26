6d0be746faae793285b0f33a7bbef57479b3adaa
NathanH <99060210@mydavinci.nl>
2019-09-26 17:43:03 +0200

    the game nathan

---
+++ Survival.js
@@ -0,0 +1,129 @@
+function myfunction(){
+	var age = prompt("hoe oud ben je?")
+
+	
+if (age >= 16)
+{
+alert("U heeft toegang tot de game!");
+}
+
+else
+{ 
+alert ("U heeft geen toegang tot de game!");
+	}
+	if (age <= 15)
+return false;
+
+if (age >=16)
+{
+alert("je ligt in het ziekenhuis al een paar maanden in coma. er is veel veranderd. Je klikt op het knopje om de zuster te roepen...")
+
+alert("er komt iemand aanlopen maar dat is niet de zuster...")
+
+alert("een zombie trapt de deur open en probeert je aan te vallen! je wilt jezelf verdedigen wat doe je?")
+
+}
+var v1 = prompt("je pakt een stoel en slaat de zombie dood of je blijft liggen en begint te schreeuwen? (antwoord met 'stoel'of 'schreeuwen')")
+
+
+
+if (v1 == 'stoel'){
+alert("goed gedaan! je hebt de zombie een paar beuken verkocht!")
+}
+
+ if (v1 == 'schreeuwen'){ 
+ alert("helaas, de zombie heeft je gebeten en bent zelf nu ook met het virus besmet!") 
+
+return false;
+}
+
+if (v2 = 'stoel'){
+	alert("je snapt nog steeds niet wat er aan de hand is")
+
+	alert('je besluit om naar huis te gaan en je vrouw en kinderen te zoeken. hiervoor moet je nog steeds het ziekenhuis door')
+  
+    alert('in de gang zie je overal bloed en ziet iemand stil staan in de gang. het is donker en er knipperen overal lichten. je stapt verkeerd en schreeuwt het uit van de pijn.')
+
+    alert('de persoon in de gang loopt langzaam naar je toe maar valt je niet aan net zoals de andere zombie dat deed. de zombie kijkt je aan en het lijkt alsof hij huilt.')
+
+    alert('de zombie praat daarna tegen je met een lage stem en zegt help me!')
+
+    var ant2 = prompt("je weet niet wat je moet doen. je denkt bij jezelf geef ik de zombie een high-kick of ga ik hem helpen?(antwoord met 'high-kick' of 'helpen'.)") 
+
+
+if (ant2 == 'high-kick'){
+alert("goed gedaan! je leek even een echte ninja!")
+}
+
+if (ant2 == 'helpen'){
+alert("helaas! de zombie gaf jou een high-kick!")
+return false;
+}
+if (v3 = 'high-kick')
+alert("je bent uit het ziekenhuis en ziet een auto staan. je besluit om er naar toe te lopen en dan terug te rijden naar huis.")
+
+alert("je rijd de straat in en ziet allemaal zombies staan. Op de achterbank liggen granaten. ")
+
+alert("je zet de auto op de rem en slipt zo over een paar zombies heen. je pakt de granaten en gooit ze stuk voor stuk in de groep zombies.")
+
+alert("na nog een paar zombies op hun hoofd te slaan is de straat leeg en kan je je huis betreden")
+
+alert("je trapt je deur in en ziet niemand zitten. je hebt het hele huis door gezocht en je loopt richting de kelder.")
+
+alert("je staat voor de kelder deur en hoort zacht getik op de deur...")
+
+alert("je hoort zacht gehuil en de stem lijkt op dat van je zoon")
+
+alert("je denkt bij jezelf wat het verstandigst is.")
+
+var ant3 = prompt("of je trapt de deur in waardoor de gene achter de deur gelijk knock-out ligt, of je doet de deur voorzichtig open en kijkt langzaam om de hoek. (antwoord met 'deur intrappen' of 'langzaam openen').")
+}
+
+if(ant3 == 'deur intrappen'){
+	alert("goed gedaan! het was dan wel je kind... maar ook weer niet met dat bloed overal..")
+}
+
+if(ant3 == 'langzaam openen'){
+
+	alert("helaas! je verwachte iets op ooghoogte te zien.. terwijl je zombie kind je in je enkel bijt..")
+
+return false;
+}
+
+
+if (v4 = 'deur intrappen')
+alert("je zit van wanhoop aan de eettafel voor je uit te staren tot dat je een geluid bij de deur hoort. het lijken wel een paar voetstappen... ")
+
+alert("je sneaked naar de deur en kijkt wat het is...")
+
+alert("je schrikt en ziet dat het je vrouw is samen met een paar andere mensen")
+
+alert("alleen kan je de voorkant van de mensen niet goed zien omdat ze gedraaid staan..")
+
+var ant4 = prompt("wat is het verstandigst? je komt om de hoek en begroet ze of je pakt een kapstok en geef ze alle drie een klap in hun nek (antwoord met 'begroeten' of 'klap geven'. ") 
+
+
+if(ant4 == 'begroeten'){
+alert("goed gedaan! je hebt je keukenprinses weer gevonden!")
+}
+
+if(ant4 == 'klap geven'){
+
+alert("helaas! je vrouwtje kan dan wel keukenprinses zijn, maar dat betekent niet dat ze onhandig is met een mes....")
+}
+
+
+if (v5 = 'begroeten'){
+
+alert("je hevt je vrouw gevonden!")
+
+} 
+
+
+
+
+
+
+
+
+}
