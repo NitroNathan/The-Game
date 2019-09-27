b4da5806463646f86c716fe8643b9d75619d4df2
NathanH <99060210@mydavinci.nl>
2019-09-27 13:44:22 +0200

    vragen toegevoegd
    extra vragen toegevoegd aan de game

--- Survival.js
+++ Survival.js
@@ -31,16 +31,20 @@ if (v1 == 'stoel'){
 alert("goed gedaan! je hebt de zombie een paar beuken verkocht!")
 }
 
- if (v1 == 'schreeuwen'){ 
+ else if (v1 == 'schreeuwen'){ 
  alert("helaas, de zombie heeft je gebeten en bent zelf nu ook met het virus besmet!") 
-
 return false;
 }
 
-if (v2 = 'stoel'){
-	alert("je snapt nog steeds niet wat er aan de hand is")
+else{
+	alert("dat is een incorrect antwoord!")
+return false;	
+}
 
-	alert('je besluit om naar huis te gaan en je vrouw en kinderen te zoeken. hiervoor moet je nog steeds het ziekenhuis door')
+if (v2 = 'stoel'){
+	alert("je snapt nog steeds niet wat er aan de hand is...")
+
+	alert('je besluit om naar huis te gaan en je vrouw en kind te zoeken. hiervoor moet je nog steeds het ziekenhuis door')
   
     alert('in de gang zie je overal bloed en ziet iemand stil staan in de gang. het is donker en er knipperen overal lichten. je stapt verkeerd en schreeuwt het uit van de pijn.')
 
@@ -48,17 +52,24 @@ if (v2 = 'stoel'){
 
     alert('de zombie praat daarna tegen je met een lage stem en zegt help me!')
 
-    var ant2 = prompt("je weet niet wat je moet doen. je denkt bij jezelf geef ik de zombie een high-kick of ga ik hem helpen?(antwoord met 'high-kick' of 'helpen'.)") 
+    var ant2 = prompt("je weet niet wat je moet doen. je denkt bij jezelf geef ik de zombie een high-kick of ga ik hem helpen?(antwoord met 'high kick' of 'helpen'.)") 
 
 
-if (ant2 == 'high-kick'){
+if (ant2 == 'high kick'){
 alert("goed gedaan! je leek even een echte ninja!")
 }
 
-if (ant2 == 'helpen'){
+else if (ant2 == 'helpen'){
 alert("helaas! de zombie gaf jou een high-kick!")
 return false;
 }
+
+else{
+	alert("dat is een incorrect antwoord!")
+return false;
+}
+
+
 if (v3 = 'high-kick')
 alert("je bent uit het ziekenhuis en ziet een auto staan. je besluit om er naar toe te lopen en dan terug te rijden naar huis.")
 
@@ -83,11 +94,12 @@ if(ant3 == 'deur intrappen'){
 	alert("goed gedaan! het was dan wel je kind... maar ook weer niet met dat bloed overal..")
 }
 
-if(ant3 == 'langzaam openen'){
-
+else if(ant3 == 'langzaam openen'){
 	alert("helaas! je verwachte iets op ooghoogte te zien.. terwijl je zombie kind je in je enkel bijt..")
-
-return false;
+	return false;
+}
+else{
+	alert("dat is een incorrect antwoord! ")
 }
 
 
@@ -107,17 +119,118 @@ if(ant4 == 'begroeten'){
 alert("goed gedaan! je hebt je keukenprinses weer gevonden!")
 }
 
-if(ant4 == 'klap geven'){
+else if(ant4 == 'klap geven'){
 
 alert("helaas! je vrouwtje kan dan wel keukenprinses zijn, maar dat betekent niet dat ze onhandig is met een mes....")
+}
+else{
+	alert("das niet het goede antwoord!")
+	return false;
 }
 
 
 if (v5 = 'begroeten'){
 
-alert("je hevt je vrouw gevonden!")
+alert("je hebt je vrouw gevonden! je vraagt aan haar wat er is gebeurd.")
 
-} 
+alert("je vrouw legt uit dat er een virus is uitgebroken en iedereen veranderd is in zombies...")
+
+alert("je vraagt je vrouw waarom zij je niet kwam opzoeken in het ziekenhuis")
+
+alert("je vrouw antwoord daar op dat ze de deur had geblokkeerd en je niet wakker kreeg. je vrouw verteld ook wat er met je zoon is gebeurd...")
+
+alert("hij kwam terug van schoolreis en voelde zich niet lekker.. hij liet de beet zien en ik moest hem wel opsluiten in de kelder")
+
+alert("je vrouw laat een bijt wond zien op haar arm en zegt dat je je geen zorgen hoeft te maken")
+
+alert("ze verteld je dat er in haar bloed stoffen zitten die tegen het virus kunnen. ze vraagt je daarom om haar te helpen en haar veilig naar het CDC te brengen.")
+
+var ant5 = prompt("wat doe je? je brengt je vrouw veilig naar het CDC? of je gaat je eigen pad? (antwoord met 'brengen' of 'eigen pad').")
+}
+
+if (ant5 == 'brengen'){
+
+alert("je hebt besloten om je vrouw te beschermen en veilig naar het CDC te brengen voor een vaccin..")
+}
+ else if (ant5 == 'eigen pad'){
+
+alert("je kiest er voor om je eigen pad te gaan en je vrouw achter te laten met onbekende mensen....")
+
+alert("je gaat je eigen pad en wenst je vrouw al het geluk. je vrouw zegt niks en kijkt je aan samen met de 2 mannen die naast haar staan.")
+
+alert("je pakt de auto waar je mee aankwam en besluit je vader en moeder te gaan zoeken")
+
+alert("de kijkt in de acteruit rij spiegel en ziet je vrouw met de mannen je na kijken...je beseft nog steeds niet helemaal wat er is gebeurd.")
+
+alert("na een lange rit stop je bij een tankstation je ziet daar 1 auto staan met allemaal zombies erom heen. je loopt via de achterkant van het tankstation en slaat met een ijzeren buis op een hek.")
+
+alert("de zombies rennen met een volle snelheid op het hek af en jij trekt de man uit de auto en sleept hem het tankstation in.")
+
+var v8 = prompt ("de man beweegt niet meer maar hij ademt nog wel. wat doe je? je gaat hem reanimeren of je laat hem liggen? (antwoord met 'reanimeren'of 'laten liggen'.") 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ if (ant5 == 'brengen'){
+
+	alert("samen met de 2 mannen en je vrouw loop je richting de auto. de auto is gepanserd en zombie bestendig. je zoekt op de kaarten uit  waar het CDC ligt.")
+
+	alert("je vraagt aan de mannen ho ze heten en vertellen je niks... ze zitten voor zich uit te staren en maken geen beweging")
+
+	alert("de man die aan het rijden is neemt een andere afslag dan jij in je gedachten had en verteld hem te stoppen en om te keren.")
+
+	alert("de man draait zich om en kijkt je diep aan in je ogen...")
+
+	alert("je ziet iets raars bewegen in zn oog... het lijkt wel een soort worm...")
+
+	alert("je doet de deur open en stapt uit de auto maar je vrouw staat daar en houd je tegen...")
+
+	alert("ze kijkt je aan en zegt; we gaan niet naar het CDC.. dat is allemaal al vernietigd.")
+
+	alert("ook zie je in je vrouw haar ogen een soort worm zwemmen..")
+
+	alert("'ga met me mee' zegt je vrouw en pakt je hand vast... je probeert jezelf los te wringen maar haar hand gaat niet los")
+
+	var v6a1k1 = prompt("wat doe je? ga je met haar mee? of geef je haar een hoek en ren je weg? (antwoord met 'meegaan' of 'hoek geven'")
+
+	if (v6a1k1 = 'meegaan')
+	alert("je vetrouwde je vrouw... die helemaal niet tegen het zombie virus bestendig was...je bent gebeten en leeft nu door als zombie.")
+
+    else if (v6a1k2 = 'hoek geven')
+    	alert("je geeft je zombie vrouw een hoek en ze gaat knock-out op de grond. toch is je vrouw onsterfelijk en pakken de 2 bewakers van je vrouw je en bijten je dood..")
+}
+
+else{
+	alert("das niet het goede antwoord!")
+	return false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -127,3 +240,47 @@ alert("je hevt je vrouw gevonden!")
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
